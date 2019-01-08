8b3a1a28623d65a48849b7c76688dc07b690a170
limeng <limeng@LAPTOP-874V2RH3>
2019-01-08 17:02:15 +0800

    第十次

--- src/main/java/me/zingon/Test/controller/TestControtller.java
+++ src/main/java/me/zingon/Test/controller/TestControtller.java
@@ -25,7 +25,7 @@ public class TestControtller {
 			if (token != null) {
 				return testService.listAll();
 			}
-			return "谢谢1234567";
+			return "谢谢12345678";
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "对不起";
