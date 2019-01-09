277a91a771c55643a6b05cdadbdf034c0e8624f4
limeng <limeng@LAPTOP-874V2RH3>
2019-01-09 16:12:34 +0800

    开始部署

--- src/main/java/me/zingon/Test/controller/TestControtller.java
+++ src/main/java/me/zingon/Test/controller/TestControtller.java
@@ -25,7 +25,7 @@ public class TestControtller {
 			if (token != null) {
 				return testService.listAll();
 			}
-			return "谢谢1";
+			return "谢谢，部署成功";
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "对不起";
