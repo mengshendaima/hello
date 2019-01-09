0f2b8560b54835ec6ce8a9cdfea962d7f25ff785
limeng <limeng@LAPTOP-874V2RH3>
2019-01-09 09:51:49 +0800

    新的一天，再来一次。

--- src/main/java/me/zingon/Test/controller/TestControtller.java
+++ src/main/java/me/zingon/Test/controller/TestControtller.java
@@ -25,7 +25,7 @@ public class TestControtller {
 			if (token != null) {
 				return testService.listAll();
 			}
-			return "谢谢12345678";
+			return "谢谢123456789";
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "对不起";
